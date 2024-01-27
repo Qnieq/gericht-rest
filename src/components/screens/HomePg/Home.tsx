82ddc62facc09153dc961b77274f5f3be7e97526
Mike <misha.o.son@gmail.com>
2024-01-27 19:27:32 +0300

    site

--- src/components/screens/HomePg/Home.tsx
+++ src/components/screens/HomePg/Home.tsx
@@ -1,9 +1,11 @@
 import AllMenus from "../../../ui/all_menus/AllMenus";
+import Awards from "../../../ui/awards/Awards";
 import BookATable from "../../../ui/book_a_table/BookATable";
 import Chef from "../../../ui/chef/Chef";
 import Customers from "../../../ui/customers/Customers";
 import Header from "../../../ui/header/Header";
 import Special from "../../../ui/special/Special";
+import VideoSection from "../../../ui/video_section/VideoSection";
 import About_History from "./About_History/About_History";
 import HomeHero from "./homeHero/HomeHero";
 
@@ -18,6 +20,8 @@ const Home = () => {
             <Special />
             <Chef />
             <Customers />
+            <VideoSection />
+            <Awards />
         </>
     );
 }
---
+++ src/ui/video_section/VideoSection.tsx
@@ -0,0 +1,47 @@
+import { useRef, useState } from 'react';
+import styles from "./VideoSection.module.scss"
+const VideoSection = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    const togglePlay = () => {
+        const video = videoRef.current;
+        if (video) {
+            if (video.paused) {
+                video.play();
+                setIsPlaying(true);
+            } else {
+                video.pause();
+                setIsPlaying(false);
+            }
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.player}>
+                {isPlaying ?
+                    <video ref={videoRef} controls className={styles.video} >
+                        <source src="videos/gericht_rest.mp4" type="video/mp4" />
+                    </video>
+                    :
+                    <video ref={videoRef} className={styles.video} >
+                        <source src="videos/gericht_rest.mp4" type="video/mp4" />
+                    </video>
+                }
+                <button onClick={togglePlay} className={styles.play_pause}>
+                    {isPlaying ?
+                        <></>
+                        :
+                        <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 120 120" fill="none">
+                            <circle cx="60" cy="60" r="59.5" stroke="#DCCA87" />
+                            <path d="M76 61.5L52 76V47L76 61.5Z" fill="white" />
+                        </svg>
+                    }
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default VideoSection;
