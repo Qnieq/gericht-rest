6c6318868830d4e3a324adb8af90276d908b7e81
Mike <misha.o.son@gmail.com>
2024-03-08 21:30:20 +0300

    analitycs

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import '../src/assets/gl_styles/_global.scss'
 import Routing from './components/Routing/Routing'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <Routing />
+      <Analytics />
     </Provider>
 )
