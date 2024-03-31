8c9802a8e929b7fd57f883f6acdab31f7fcc11b1
Mike <misha.o.son@gmail.com>
2024-03-31 17:07:20 +0300

    env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_DATABASE_API_KEY,
-  authDomain: process.env.VITE_DATABASE_AUTH_DOMAIN,
-  databaseURL: process.env.VITE_DATABASE_URL,
-  projectId: process.env.VITE_DATABASE_PROJECT_ID,
-  storageBucket: process.env.VITE_DATABASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_DATABASE_MESSAGERING_SENDER_ID,
-  appId: process.env.VITE_DATABASE_APP_ID,
-  measurementId: process.env.VITE_DATABASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_DATABASE_API_KEY,
+  authDomain: import.meta.env.VITE_DATABASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_DATABASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_DATABASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_DATABASE_MESSAGERING_SENDER_ID,
+  appId: import.meta.env.VITE_DATABASE_APP_ID,
+  measurementId: import.meta.env.VITE_DATABASE_MEASUREMENT_ID
 };
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    VITE_DATABASE_API_KEY: process.env.VITE_DATABASE_API_KEY ,
-    VITE_DATABASE_AUTH_DOMAIN: process.env.VITE_DATABASE_AUTH_DOMAIN,
-    VITE_DATABASE_URL: process.env.VITE_DATABASE_URL,
-    VITE_DATABASE_PROJECT_ID: process.env.VITE_DATABASE_PROJECT_ID,
-    VITE_DATABASE_STORAGE_BUCKET: process.env.VITE_DATABASE_STORAGE_BUCKET,
-    VITE_DATABASE_MESSAGERING_SENDER_ID: process.env.VITE_DATABASE_MESSAGERING_SENDER_ID,
-    VITE_DATABASE_APP_ID: process.env.VITE_DATABASE_APP_ID,
-    VITE_DATABASE_MEASUREMENT_ID: process.env.VITE_DATABASE_MEASUREMENT_ID
-  },
 })
